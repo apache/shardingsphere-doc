9a88ce9f768d8d6a2c691a022a46d52c37d00ce1
liang <1035847702@qq.com>
2021-07-02 20:22:22 +0800

    update repo

--- .github/scripts/build-website.sh
+++ .github/scripts/build-website.sh
@@ -28,8 +28,7 @@ export TZ="Asia/Shanghai"
 echo "[1] ====>>>> process shardingsphere/docs"
 echo git clone https://github.com/apache/shardingsphere
 
-# git clone https://github.com/apache/shardingsphere _shardingsphere 
-git clone https://github.com/QiliangFan/shardingsphere.git _shardingsphere 
+git clone https://github.com/apache/shardingsphere _shardingsphere 
 
 echo check diff
 if  [ ! -s old_version_ss ]  ; then

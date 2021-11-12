f0a38f0d2055a7a2f56b7d54bd49011884b9f0de
Haoran Meng <menghaoranss@gmail.com>
2021-11-12 18:10:28 +0800

    Update conf.py

--- .github/scripts/conf.py
+++ .github/scripts/conf.py
@@ -64,8 +64,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-#latex_engine = 'xelatex'
-latex_engine = 'pdflatex'
+latex_engine = 'xelatex'
 
 latex_elements = {
     'papersize': 'a4paper',

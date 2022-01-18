daebd01d607b3cac94fb3e8693c94a22b6ebb85c
Haoran Meng <menghaoranss@gmail.com>
2022-01-18 15:44:11 +0800

    Update generate-pdf.sh

--- .github/scripts/generate-pdf.sh
+++ .github/scripts/generate-pdf.sh
@@ -10,7 +10,7 @@ function prepare {
     sudo apt-get update -y 
     sudo apt-get install -y latexmk texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended
     sudo apt-get install texlive-xetex latex-cjk-all
-    wget -q https://github.com/jgm/pandoc/releases/download/2.16.1/pandoc-2.16.1-1-amd64.deb
+    #wget -q https://github.com/jgm/pandoc/releases/download/2.16.1/pandoc-2.16.1-1-amd64.deb
     sudo dpkg -i pandoc-2.16.1-1-amd64.deb
     #wget https://github.com/adobe-fonts/source-han-sans/raw/release/OTF/SourceHanSansSC.zip
     wget https://github.com/adobe-fonts/source-han-sans/releases/latest/download/SourceHanSansSC.zip

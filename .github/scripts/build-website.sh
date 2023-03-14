a26201657f5ab32cbad3b6d1dc0f2ccecfd15fa5
孙念君 Nianjun Sun <sunnianjun@apache.org>
2023-03-14 18:51:03 +0800

    Add : add show up the path content

--- .github/scripts/build-website.sh
+++ .github/scripts/build-website.sh
@@ -85,7 +85,7 @@ diff ../old_version_ss new_version_ss > result_version
     mv docs ssdocs
     
     echo build hugo ss documents
-    cd document && docker run --volume $(pwd):/opt/input  docker-hugo:latest
+    cd document && ls . && docker run --volume $(pwd):/opt/input  docker-hugo:latest
     cd ..
     sh ./ssdocs/build-with-docker.sh
     cp -rf ssdocs/target ./

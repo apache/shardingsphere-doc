18f4aa2e15369a0abbbd09a3104f656c67d574c0
jinnanjiang <15808034733@163.com>
2026-01-12 19:04:19 +0800

    Feat: ADD Kapa-AI (#754)

---
+++ js/kapa-widget.js
@@ -0,0 +1,13 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://widget.kapa.ai/kapa-widget.bundle.js';
+  script.setAttribute('data-website-id', 'c939d944-e3f7-455e-9576-9707174fda5d');
+  script.setAttribute('data-project-name', 'ShardingSphere-Widget');
+  script.setAttribute('data-project-logo', 'https://shardingsphere.apache.org/images/base/logo.png');
+  script.setAttribute('data-button-hide', 'false');
+  script.setAttribute('data-bot-protection-mechanism', 'hcaptcha');
+  script.setAttribute('data-modal-open-on-command-k', 'true');
+  script.setAttribute('data-modal-title', 'Apache ShardingSphere AI');
+  document.head.appendChild(script);
+})();

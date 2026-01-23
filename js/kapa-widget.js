a7e60b475debc11a9b6d9bca047def014ef79a32
jinnanjiang <15808034733@163.com>
2026-01-23 14:09:51 +0800

    Kapa-widget script Enable Privacy Policy confirmation

--- js/kapa-widget.js
+++ js/kapa-widget.js
@@ -9,5 +9,7 @@
   script.setAttribute('data-bot-protection-mechanism', 'hcaptcha');
   script.setAttribute('data-modal-open-on-command-k', 'true');
   script.setAttribute('data-modal-title', 'Apache ShardingSphere AI');
+  script.setAttribute('data-consent-required', 'true');
+  script.setAttribute('data-consent-screen-disclaimer', 'By clicking "I agree, let\'s chat", you consent to the use of the AI assistant in accordance with kapa.ai\'s [Privacy Policy](https://www.kapa.ai/content/privacy-policy). This service uses reCAPTCHA, which requires your consent to Google\'s [Privacy Policy](https://policies.google.com/privacy) and [Terms of Service](https://policies.google.com/terms). By proceeding, you explicitly agree to both kapa.ai\'s and Google\'s privacy policies.');
   document.head.appendChild(script);
 })();
